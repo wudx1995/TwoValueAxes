7a34ac72b6f12af476f9f1eba79a41982cde71f3
wudx1995 <91259827+wudx1995@users.noreply.github.com>
2023-11-17 10:31:06 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,89 +1,82 @@
 var getScriptPromisify = (src) => {
-  return new Promise((resolve) => {
-    $.getScript(src, resolve);
-  });
-};
+  return new Promise(resolve => {
+    $.getScript(src, resolve)
+  })
+}
 
 (function () {
-  const prepared = document.createElement("template");
+  const prepared = document.createElement('template')
   prepared.innerHTML = `
       <style>
       </style>
       <div id="root" style="width: 100%; height: 100%;">
       </div>
-    `;
+    `
   class SamplePrepared extends HTMLElement {
-    constructor() {
-      super();
+    constructor () {
+      super()
 
-      this._shadowRoot = this.attachShadow({ mode: "open" });
-      this._shadowRoot.appendChild(prepared.content.cloneNode(true));
+      this._shadowRoot = this.attachShadow({ mode: 'open' })
+      this._shadowRoot.appendChild(prepared.content.cloneNode(true))
 
-      this._root = this._shadowRoot.getElementById("root");
+      this._root = this._shadowRoot.getElementById('root')
 
-      this._props = {};
+      this._props = {}
 
-      this.render();
+      this.render()
     }
 
-    onCustomWidgetResize(width, height) {
-      this.render();
+    onCustomWidgetResize (width, height) {
+      this.render()
     }
 
-<<<<<<< Updated upstream
     async render () {
       await getScriptPromisify('https://wudx1995.github.io/TwoValueAxes/echarts.min.js')
-=======
-    async render() {
-      await getScriptPromisify(
-        "https://wudx1995.github.io/TwoValueAxes/echarts.min.js"
-      );
->>>>>>> Stashed changes
 
-      const chart = echarts.init(this._root);
+      const chart = echarts.init(this._root)
       const data = [];
-      for (let i = 0; i <= 360; i++) {
-        let t = (i / 180) * Math.PI;
-        let r = Math.sin(2 * t) * Math.cos(2 * t);
-        data.push([r, i]);
-      }
-      const option = {
-        title: {
-          text: "",
-        },
-        legend: {
-          data: ["line"],
-        },
-        polar: {
-          center: ["50%", "54%"],
-        },
-        tooltip: {
-          trigger: "axis",
-          axisPointer: {
-            type: "cross",
-          },
-        },
-        angleAxis: {
-          type: "value",
-          startAngle: 0,
-        },
-        radiusAxis: {
-          min: 0,
-        },
-        series: [
-          {
-            coordinateSystem: "polar",
-            name: "line",
-            type: "line",
-            showSymbol: false,
-            data: data,
-          },
-        ],
-        animationDuration: 2000,
-      };
-      chart.setOption(option);
+		for (let i = 0; i <= 360; i++) {
+		  let t = (i / 180) * Math.PI;
+		  let r = Math.sin(2 * t) * Math.cos(2 * t);
+		  data.push([r, i]);
+		}
+		const option = {
+		  title: {
+			text: ''
+		  },
+		  legend: {
+			data: ['line']
+		  },
+		  polar: {
+			center: ['50%', '54%']
+		  },
+		  tooltip: {
+			trigger: 'axis',
+			axisPointer: {
+			  type: 'cross'
+			}
+		  },
+		  angleAxis: {
+			type: 'value',
+			startAngle: 0
+		  },
+		  radiusAxis: {
+			min: 0
+		  },
+		  series: [
+			{
+			  coordinateSystem: 'polar',
+			  name: 'line',
+			  type: 'line',
+			  showSymbol: false,
+			  data: data
+			}
+		  ],
+		  animationDuration: 2000
+		};
+      chart.setOption(option)
     }
   }
 
-  customElements.define("com-sap-sample-echarts-prepared", SamplePrepared);
-})();
+  customElements.define('com-sap-sample-echarts-prepared', SamplePrepared)
+})()
